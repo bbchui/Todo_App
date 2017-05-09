d0a56df72f638d036610c262ded1224cb302c552
App Academy Student <student@appacademy.io>
2017-05-08 18:07:13 -0700

    fix delete button

--- frontend/components/todo_list/todo_list_item.jsx
+++ frontend/components/todo_list/todo_list_item.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TodoListItem = ({todo, removeTodo}) => (
   <li>{todo.title}
-    <button onClick={removeTodo(todo.id)}>delete!</button>
+    <button onClick={() => removeTodo(todo)}>delete!</button>
   </li>
 );
 
